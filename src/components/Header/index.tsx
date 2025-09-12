418141bdbbcb3a5b38800f958fc7bcb45e17dc29
van-gomes <nninha.gomes@gmail.com>
2025-09-12 16:31:11 -0300

    feat: create new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
-import logoImg from "../../assets/logo.svg";
+import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
     return (
@@ -14,17 +15,9 @@ export function Header() {
                         <NewTransactionButton>Nova transação</NewTransactionButton>
                     </Dialog.Trigger>
 
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-
-                        <Dialog.Content>
-                            <Dialog.Title>Nova Transação</Dialog.Title>
-
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal />
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
-    );
+    )
 }
