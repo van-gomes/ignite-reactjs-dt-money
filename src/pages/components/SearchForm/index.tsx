6f5cd12042fc0d8f3d514434f4467367904c4d37
van-gomes <nninha.gomes@gmail.com>
2025-09-22 15:17:54 -0300

    feat: search for transactions with errors

--- src/pages/components/SearchForm/index.tsx
+++ src/pages/components/SearchForm/index.tsx
@@ -1,8 +1,10 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 import { MagnifyingGlass } from "phosphor-react";
-import { SearchFormContainer } from "./styles";
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext);
+
     const {
         register,
         handleSubmit,
@@ -20,9 +24,7 @@ export function SearchForm() {
     });
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-
-        console.log(data);
+        await fetchTransactions(data.query)
     }
 
     return (
